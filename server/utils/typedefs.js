5c95b6b76f481ab2fd35f8410af15f617cce520d
Bryanna Vinge <bevinge711@gmail.com>
2021-11-29 15:41:03 -0600

    added utils

---
+++ server/utils/typedefs.js
@@ -0,0 +1,36 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+  type User {
+    _id: ID
+    username: String
+    email: String
+    password: String
+    companyName: String
+    bio: String
+    photoType: String
+    location: String
+    link: String
+    reservationCost: String
+    image: String
+    
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  type Query {
+    users: (location: String!) : [User]
+    user(username: String!): User
+
+
+  type Mutation {
+    addUser(username: String!, email: String!, password: String!): Auth
+    login(email: String!, password: String!): Auth
+  }
+`;
+
+module.exports = typeDefs;
+
